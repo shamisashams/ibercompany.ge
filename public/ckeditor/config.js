002a545f6dfa49afbfe1fe051aff6bd9c4f2bbfb
shamisashams <shamisaashams@gmail.com>
2021-08-31 15:25:40 +0400

    font and padding deduction

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -3,8 +3,11 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    // config.font_names = "tbc-regular";
+
+    config.font_defaultLabel = "tbc-regular";
 };
